35dbedc8d3889190a8ad689c2b7f9d52069a44c6
Sneaky-petrodectyl <nileshjha412@gmail.com>
2018-01-24 21:21:12 +0530

    Implement dumy signup route

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,13 +1,27 @@
 from src import app
-# from flask import jsonify
+from flask import Flask, render_template, request, make_response, send_file
+import requests
+import json
 
+authUrl = "https://auth.butane33.hasura-app.io/v1/signup"
 
 @app.route("/")
 def home():
     return "Hasura Hello World"
 
-# Uncomment to add a new URL at /new
+@app.route('/signup', methods=['POST'])
+def zomatoSignup():
+    userData = request.get_json()
+    requestPayload = {
+        "provider": "username",
+        "data": {
+            "username": userData['username'],
+            "password": userData['username']
+        }
+    }
+    headers = {
+    "Content-Type": "application/json"
+    }
+    resp = requests.request("POST", url, data=json.dumps(requestPayload), headers=headers).json()
+    return jsonify({"auth_token" : resp['auth_token']})
 
-# @app.route("/json")
-# def json_message():
-#     return jsonify(message="Hello World")
